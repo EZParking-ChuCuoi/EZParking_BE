6eafcc9cb5091983950faedb623b50f85398bb30
congbio <acongkplpe@gmail.com>
2023-03-01 12:03:02 +0700

    swagger document api

--- app/Http/Controllers/Profile/UserController.php
+++ app/Http/Controllers/Profile/UserController.php
@@ -15,32 +15,111 @@ class UserController extends Controller
 {
     public function __construct(
         private readonly IProfile $profileService,
-    ) {}
-    public function getAllUser(){
-        $userData=$this->profileService->getAllUser();
+    ) {
+    }
+    /**
+     * @OA\Get(
+     ** path="/api/user/", tags={"User"}, summary="get all user", operationId="getAllUser",
+     *   @OA\Response( response=201, description="Success",@OA\MediaType(mediaType="application/json",)),
+     *   @OA\Response( response=401, description="Unauthenticated"
+     *   ),
+     *   @OA\Response( response=400, description="Bad Request"
+     *   ),
+     *   @OA\Response( response=404, description="not found"
+     *   ),
+     *    @OA\Response( response=403, description="Forbidden"
+     *      ),
+     *  * security={ {"passport":{}}}
+     *)
+     **/
+    public function getAllUser()
+    {
+        $userData = $this->profileService->getAllUser();
         return $userData;
     }
-    public function getRole($id){
+    /**
+     * @OA\Get(
+     ** path="/api/user/{id}/role", tags={"User"}, summary="check role user", operationId="getRole",
+     *   @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="ID of the user to retrieve",
+     *         @OA\Schema(type="integer")
+     *     ),
+     *@OA\Response( response=403, description="Forbidden"),
+     * security={ {"passport":{}}}
+     *)
+     **/
+    public function getRole($id)
+    {
         $role['role'] = User::find($id)->role;
         return $role;
     }
-    public function showProfile($id){
-        $userInfo= $this->profileService->show($id);
-        if($userInfo == [null]){
+    public function showProfile($id)
+    {
+        $userInfo = $this->profileService->show($id);
+        if ($userInfo == [null]) {
             return $this->responseError(
                 "User not exit !",
                 Response::HTTP_BAD_REQUEST,
             );
-        }
-        else{
+        } else {
             return $this->responseSuccessWithData(
-            "Infomation of user",
-            [$userInfo],
-            Response::HTTP_ALREADY_REPORTED
-        );
+                "Infomation of user",
+                [$userInfo],
+                Response::HTTP_ALREADY_REPORTED
+            );
         }
     }
-    public function updateProfile(Request $request,$id){
+    /**
+     * Update the user's profile.
+     *
+     * @OA\Post(
+     *     path="/api/user/update/{id}",
+     *     summary="Update user profile",
+     *     tags={"User"},
+     *     operationId="updateProfile",
+     *     @OA\Parameter(
+     *         name="id",
+     *         description="User ID",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int64",
+     *         )
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="multipart/form-data",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="fullName",
+     *                     type="string",
+     *                 ),@OA\Property(
+     *                     property="_method",
+     *                     type="string",
+     *                      example="PUT"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="avatar",
+     *                     type="file"
+     *                 )
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Profile updated successfully"
+     *     ),
+     * security={ {"passport":{}}}
+     * 
+     * )
+     */
+    public function updateProfile(Request $request, $id)
+    {
         $validator = Validator::make($request->all(), [
             'fullName' => 'required',
             'avatar' => 'required|image|max:2048',
@@ -50,19 +129,16 @@ class UserController extends Controller
         }
         $dateData = $validator->validated();
 
-        $user= User::find($id);
+        $user = User::find($id);
 
 
-        $user->fullName=$dateData["fullName"];
+        $user->fullName = $dateData["fullName"];
         if ($request->hasFile('avatar')) {
             $file   = $request->file('avatar');
-            $linkImage =CloudinaryStorage::upload($file->getRealPath(), $file->getClientOriginalName(),'account/profile'); 
+            $linkImage = CloudinaryStorage::upload($file->getRealPath(), $file->getClientOriginalName(), 'account/profile');
             $user->avatar = $linkImage;
         }
         $user->save();
-        return $this->responseSuccessWithData("update success",[$user],Response::HTTP_OK);
-
+        return $this->responseSuccessWithData("update success", [$user], Response::HTTP_OK);
     }
-   
-
 }
