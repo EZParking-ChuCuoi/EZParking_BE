8baa3392f04248988f5028cbb3db7ae094c927eb
congbio <acongkplpe@gmail.com>
2023-03-12 12:49:40 +0700

    feat statics revenue each parking lot

--- app/Http/Controllers/DashBoard/ManagerController.php
+++ app/Http/Controllers/DashBoard/ManagerController.php
@@ -18,6 +18,127 @@ class ManagerController extends Controller
 {
     /**
      * @OA\Get(
+     ** path="/api/dashboard/parkingLots/{parkingLotId}/{period}", tags={"DashBoard"}, summary="get statics of one parkingLot with a Id",
+     * operationId="parkingLotBookingStats",
+     *   @OA\Parameter(
+     *         name="parkingLotId",
+     *         in="path",
+     *         required=true,
+     *         description="id parkingLot management",
+     *         example=1000000,
+     *         @OA\Schema(type="integer"),
+     *     ),
+     *     @OA\Parameter(
+     *         name="period",
+     *         in="path",
+     *         required=true,
+     *         description="period day or week or month or year",
+     *         example="day",
+     *         @OA\Schema(type="string"),
+     *     ),
+     *@OA\Response( response=403, description="Forbidden"),
+     * security={ {"passport":{}}}
+     *)
+     **/
+    public function parkingLotBookingStats($parkingLotId, $period)
+    {
+        $now = Carbon::now();
+    
+
+        if ($period == 'day') {
+            $end = $now->toDateString();
+            $start = $now->startOfMonth()->toDateString();
+            $groupBy = DB::raw('Date(bookDate)');
+            $format = 'Y-m-d';
+        }elseif ($period == 'week') {
+            $end = $now->toDateString();
+            $start = $now->startOfMonth()->toDateString();
+            // return $start."............".$end;
+            $groupBy = DB::raw('WEEK(bookDate)');
+            $format = 'W';
+        }
+        elseif ($period == 'month') {
+            $end = $now->toDateString();
+            $start = $now->startOfYear()->toDateString();
+            $groupBy = DB::raw('DATE_FORMAT(bookDate, "%Y-%m")');
+            $format = 'Y-m';
+        } elseif ($period == 'year') {
+            $end = $now->endOfYear()->format('Y-m-d');
+            $start = $now->subYears(5)->format('Y-m-d');
+            // return $start. ' .........'.$end;
+            $groupBy = DB::raw('YEAR(bookDate)');
+            $format = 'Y';
+        } else {
+            return response()->json([
+                'message' => "Invalid period specified.",
+                'data' => null
+            ], 400);
+        }
+        // Get the parking lot ids for the given user
+        $parkingLotInfo = ParkingLot::where('id', $parkingLotId)->exists();
+
+        if (!$parkingLotInfo) {
+            return response()->json([
+                'message' => "No parkingLot with this Id.",
+                'data' => null
+            ], 404);
+        }
+        // Get the block ids in the parking lots for the given user
+        $blockIdsInParkingLots = Block::where('parkingLotId', $parkingLotId)->pluck('id');
+
+        // Get the slot ids in the blocks for the given user
+        $slotIdsInBlocks = ParkingSlot::whereIn('blockId', $blockIdsInParkingLots)->pluck('id');
+
+
+        $bookings= Booking::whereIn('slotId',$slotIdsInBlocks)
+        ->select(
+            DB::raw("{$groupBy} as period"),
+            DB::raw('SUM(payment) as total_sales'),
+            DB::raw('COUNT(DISTINCT  userId) as total_users'),
+            DB::raw('COUNT(DISTINCT bookDate) as total_bookings'),
+ 
+        )
+        ->whereBetween('bookDate', [$start, $end])
+        ->groupBy($groupBy)
+        ->get();
+        if ($bookings->isEmpty()) {
+            return response()->json([
+                'message' => "No bookings data available for the specified period.",
+                'data' => null
+            ], 404);
+        }
+
+        $start = Carbon::parse($bookings->first()->period)->startOf($period);
+        $end = Carbon::parse($bookings->last()->period)->endOf($period);
+        $periods = $this->getPeriods($start, $end, $format, $period);
+        $bookings = $this->fillMissingPeriods($periods, $bookings, $groupBy->getValue());
+
+        $periodLabels = $bookings->pluck('period')->toArray();
+        $salesTotals = $bookings->pluck('total_sales')->toArray();
+        $uniqueUsers = $bookings->pluck('total_users')->toArray();
+        foreach ($uniqueUsers as &$value) {
+            if (is_null($value)) {
+                $value = 0;
+            }
+        }
+        $bookingCounts = $bookings->pluck('total_bookings')->toArray();
+        foreach ($bookingCounts as &$value) {
+            if (is_null($value)) {
+                $value = 0;
+            }
+        }
+        return response()->json([
+            'message' => "Success!",
+            'data' => [
+                'periodLabels' => $periodLabels,
+                'salesTotals' => $salesTotals,
+                'uniqueUsers' => $uniqueUsers,
+                'bookingCounts' => $bookingCounts,
+            ]
+        ], 200); // Return a JSON response with the sales data
+    }
+    /**
+     * @OA\Get(
      ** path="/api/dashboard/parkingLots/{userId}", tags={"DashBoard"}, summary="get all parkinglot by user management",
      * operationId="getParkingUserManage",
      *   @OA\Parameter(
